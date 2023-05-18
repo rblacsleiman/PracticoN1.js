3bf75826e8d24b1d555c66dfac8ae831faaa4f3e
Rosario Blac Sleiman <rblacsleiman@gmail.com>
2023-05-17 22:56:08 -0300

    Def de variables

---
+++ js/1-variables.js
@@ -0,0 +1,23 @@
+// comentario de una sola linea
+
+/* comentario
+de
+multiples
+lineas
+ */
+
+ //variables : espacio de memoria que almacena un dato. (var - let - const)
+
+let  producto = 'ipad pro'; //les puedo cambiar el valor en un futuro 
+let precio = 700;
+let luzEncendida = false;
+    
+// no puedo crear variables con el mismo nombre
+
+//mostrar en el documento html 
+document.write('Hola Mundo');
+document.write(precio * 380); // como no puse la palabra precio entre comillas , hago alusion a la variable
+
+
+
+
